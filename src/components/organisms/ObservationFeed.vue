51821e51d0936847c01969dfada79935a5488f30
kissmygritts <kissmygritts08@gmail.com>
2019-08-06 14:43:22 -0700

    Feature: add apollo and tailwind css to project

---
+++ src/components/organisms/ObservationFeed.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="observation-feed">
+    <pre class="bg-gray-400">
+      <code class="text-indigo-800">
+        {{ observationFeed }}
+      </code>
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ObservationFeed',
+  props: [ 'observationFeed' ]
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createProvider } from './vue-apollo'
+import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
