2a4cac17a6756323cc4915381e0c50f2d28355d1
kissmygritts <kissmygritts08@gmail.com>
2019-10-17 15:53:57 -0700

    Feature: abillity to change the leaflet component basemap

--- src/components/organisms/observation-map.vue
+++ src/components/organisms/observation-map.vue
@@ -7,7 +7,16 @@
       v-if="showMap"
     >
       <!-- basemap layers -->
-      <l-tile-layer :url="url" />
+      <l-control-layers position="topright" />
+      <l-tile-layer
+        v-for="tileProvider in tileProviders"
+        :key="tileProvider.name"
+        :name="tileProvider.name"
+        :visible="tileProvider.visible"
+        :url="tileProvider.url"
+        :attribution="tileProvider.attribution"
+        layer-type="base"
+      />
 
       <!-- markers on the map -->
       <marker-with-tooltip
@@ -39,28 +48,61 @@
 
 <script>
 import { latLng } from 'leaflet'
-import { LMap, LTileLayer, LCircleMarker } from 'vue2-leaflet'
+import { LMap, LTileLayer, LControlLayers, LCircleMarker } from 'vue2-leaflet'
 import MarkerWithTooltip from '@/components/molecules/marker-with-tooltip.vue'
 import { EventBus } from '@/event-bus.js'
 
 export default {
   name: 'observation-map',
+
   components: {
     LMap,
     LTileLayer,
+    LControlLayers,
     LCircleMarker,
     MarkerWithTooltip
   },
+
   props: [ 'observations' ],
+
   data () {
     return {
       showMap: true,
       zoom: 7,
       center: latLng(39.55, -117.0667),
       url: 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
-      hoveredFeedCard: null
+      hoveredFeedCard: null,
+      tileProviders: [
+        {
+          name: 'Light Mode',
+          visible: true,
+          url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        }, {
+          name: 'Dark Mode',
+          visible: false,
+          url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        }, {
+          name: 'Physical Map',
+          visible: false,
+          url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}',
+          attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service'
+        }, {
+          name: 'Topo Map',
+          visible: false,
+          url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+          attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+        }, {
+          name: 'Imagery',
+          visible: false,
+          url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+          attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        }
+      ]
     }
   },
+
   computed: {
     mapPoints () {
       return this.observations.map(m => {
@@ -76,6 +118,7 @@ export default {
       return this.$refs.mapContainer.clientWidth
     }
   },
+
   methods: {
     handleMarkerClick (id) {
       console.log(id)
@@ -90,6 +133,7 @@ export default {
     //   console.log(e)
     // }
   },
+
   created () {
     this.$nextTick(() => {
       EventBus.$on('eb-drawer-slider:update', ({ visible, data }) => {
